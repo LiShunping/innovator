5cff901fc44caea83a1af33495c356d7ddf76f88
李顺平 <shunping.li@foxmail.com>
2018-11-01 02:06:32 +0800

    build: 追加 precommit 环节便于自动部署

---
+++ dist/static/js/mock.2326121d1d7438a16605.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{KMcm:function(e,a,o){"use strict";a.a={user:{login:"/userapi/front/login"}}},gEZH:function(e,a,o){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var t=o("zNUS"),s=o.n(t);s.a.setup({timeout:"100-1000"});var n=o("KMcm");s.a.mock(RegExp(n.a.user.login),{code:200,message:"ok",data:{name:"李顺平",id:1221,msgNum:2,role:"teacher"}})}},["gEZH"]);
+//# sourceMappingURL=mock.2326121d1d7438a16605.js.map
